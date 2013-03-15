15f60e5178a3c526617b6172635e43a5e71ed295
Anarchic <parkhdyl@gmail.com>
2013-03-15 04:44:06 -0400

    Ver 1.0
    Faced with the challenge of needing to create Xml Files, I couldn't find
    anything that suited my needs online. XmlNotepad was suitable for small
    tweaks and edits, but adding all those nodes manually? Forget it. So I
    made this, which will allow me to quickly add armor, weapon, potion and
    scroll entries. This will obviously not be updated as frequently as the
    main project, but it will get occasional updates as I refine the various
    features + possibly change around item data types or add new ones
    entirely.

---
+++ CronkXMLEditor/ArmorDialog.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace CronkXMLEditor
+{
+    partial class ArmorDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ElectricDefVal = new System.Windows.Forms.NumericUpDown();
+            this.FrostDefVal = new System.Windows.Forms.NumericUpDown();
+            this.AcidDefVal = new System.Windows.Forms.NumericUpDown();
+            this.FireDefVal = new System.Windows.Forms.NumericUpDown();
+            this.CrushingDefVal = new System.Windows.Forms.NumericUpDown();
+            this.PiercingDefVal = new System.Windows.Forms.NumericUpDown();
+            this.SlashingDefVal = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ElectricDefVal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FrostDefVal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AcidDefVal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FireDefVal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CrushingDefVal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PiercingDefVal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SlashingDefVal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label14);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.CloseButton);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.ElectricDefVal);
+            this.groupBox1.Controls.Add(this.FrostDefVal);
+            this.groupBox1.Controls.Add(this.AcidDefVal);
+            this.groupBox1.Controls.Add(this.FireDefVal);
+            this.groupBox1.Controls.Add(this.CrushingDefVal);
+            this.groupBox1.Controls.Add(this.PiercingDefVal);
+            this.groupBox1.Controls.Add(this.SlashingDefVal);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(208, 238);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Defense Values:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(181, 177);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(15, 13);
+            this.label14.TabIndex = 30;
+            this.label14.Text = "%";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(181, 151);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(15, 13);
+            this.label13.TabIndex = 29;
+            this.label13.Text = "%";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(181, 125);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(15, 13);
+            this.label12.TabIndex = 28;
+            this.label12.Text = "%";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(181, 99);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(15, 13);
+            this.label11.TabIndex = 27;
+            this.label11.Text = "%";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(181, 73);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(15, 13);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "%";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(181, 47);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(15, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "%";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(181, 21);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(15, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "%";
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(67, 201);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 177);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Electric:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 151);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Frost:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Acid:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Fire:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Crushing:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Piercing:";
+            // 
+            // ElectricDefVal
+            // 
+            this.ElectricDefVal.Location = new System.Drawing.Point(67, 175);
+            this.ElectricDefVal.Name = "ElectricDefVal";
+            this.ElectricDefVal.ReadOnly = true;
+            this.ElectricDefVal.Size = new System.Drawing.Size(108, 20);
+            this.ElectricDefVal.TabIndex = 17;
+            // 
+            // FrostDefVal
+            // 
+            this.FrostDefVal.Location = new System.Drawing.Point(67, 149);
+            this.FrostDefVal.Name = "FrostDefVal";
+            this.FrostDefVal.ReadOnly = true;
+            this.FrostDefVal.Size = new System.Drawing.Size(108, 20);
+            this.FrostDefVal.TabIndex = 16;
+            // 
+            // AcidDefVal
+            // 
+            this.AcidDefVal.Location = new System.Drawing.Point(67, 123);
+            this.AcidDefVal.Name = "AcidDefVal";
+            this.AcidDefVal.ReadOnly = true;
+            this.AcidDefVal.Size = new System.Drawing.Size(108, 20);
+            this.AcidDefVal.TabIndex = 15;
+            // 
+            // FireDefVal
+            // 
+            this.FireDefVal.Location = new System.Drawing.Point(67, 97);
+            this.FireDefVal.Name = "FireDefVal";
+            this.FireDefVal.ReadOnly = true;
+            this.FireDefVal.Size = new System.Drawing.Size(108, 20);
+            this.FireDefVal.TabIndex = 14;
+            // 
+            // CrushingDefVal
+            // 
+            this.CrushingDefVal.Location = new System.Drawing.Point(67, 71);
+            this.CrushingDefVal.Name = "CrushingDefVal";
+            this.CrushingDefVal.ReadOnly = true;
+            this.CrushingDefVal.Size = new System.Drawing.Size(108, 20);
+            this.CrushingDefVal.TabIndex = 13;
+            // 
+            // PiercingDefVal
+            // 
+            this.PiercingDefVal.Location = new System.Drawing.Point(67, 45);
+            this.PiercingDefVal.Name = "PiercingDefVal";
+            this.PiercingDefVal.ReadOnly = true;
+            this.PiercingDefVal.Size = new System.Drawing.Size(108, 20);
+            this.PiercingDefVal.TabIndex = 12;
+            // 
+            // SlashingDefVal
+            // 
+            this.SlashingDefVal.Location = new System.Drawing.Point(67, 19);
+            this.SlashingDefVal.Name = "SlashingDefVal";
+            this.SlashingDefVal.ReadOnly = true;
+            this.SlashingDefVal.Size = new System.Drawing.Size(108, 20);
+            this.SlashingDefVal.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Slashing:";
+            // 
+            // ArmorDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(233, 262);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ArmorDialog";
+            this.Text = "Armor Defense";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ElectricDefVal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FrostDefVal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AcidDefVal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FireDefVal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CrushingDefVal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PiercingDefVal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SlashingDefVal)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown ElectricDefVal;
+        private System.Windows.Forms.NumericUpDown FrostDefVal;
+        private System.Windows.Forms.NumericUpDown AcidDefVal;
+        private System.Windows.Forms.NumericUpDown FireDefVal;
+        private System.Windows.Forms.NumericUpDown CrushingDefVal;
+        private System.Windows.Forms.NumericUpDown PiercingDefVal;
+        private System.Windows.Forms.NumericUpDown SlashingDefVal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ CronkXMLEditor/ArmorDialog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CronkXMLEditor
+{
+    public partial class ArmorDialog : Form
+    {
+        public ArmorDialog(int AbVal, int InsVal, int PadVal, int RigVal, int HardVal)
+        {
+            InitializeComponent();
+            SlashingDefVal.Value = (HardVal * 4) + (RigVal * 2);
+            PiercingDefVal.Value = (HardVal * 4) + (PadVal * 2);
+            CrushingDefVal.Value = (RigVal * 4) + (PadVal * 2);
+            FireDefVal.Value = (AbVal * 4) + (RigVal * 2);
+            FrostDefVal.Value = (PadVal * 4) + (InsVal * 2);
+            ElectricDefVal.Value = (InsVal * 4) + (PadVal * 2);
+            AcidDefVal.Value = (InsVal * 4) + (AbVal * 2);
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
